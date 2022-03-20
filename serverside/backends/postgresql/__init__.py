21421a114a57f9090e6272e21ff094bf17b2fca7
Marcus Wörner <61889355+woernerm@users.noreply.github.com>
2022-03-20 12:11:47 +0100

    Make app for focused on db users
    Originally, serverside was also intended to create functions in the database. However, it is better to move that feature into a separate app to allow better customization and easier maintenance.

--- serverside/backends/postgresql/__init__.py
+++ serverside/backends/postgresql/__init__.py
@@ -3,11 +3,10 @@ Defines a backend class for the PostgreSQL database.
 """
 
 from serverside.backends.base import BackendBase
-from serverside.backends.postgresql.function import FunctionMixin
 from serverside.backends.postgresql.user import UserMixin
 
 
-class PostgreSQL(UserMixin, FunctionMixin, BackendBase):
+class PostgreSQL(UserMixin, BackendBase):
     """
     Backend class for PostgreSQL.
     """
--- tests/backends/postgresql/data/increment.sql
+++
@@ -1,5 +0,0 @@
-CREATE FUNCTION increment(i integer) RETURNS integer AS $$
-        BEGIN
-                RETURN i + 1;
-        END;
-$$ LANGUAGE plpgsql;
